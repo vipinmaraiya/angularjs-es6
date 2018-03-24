eaa8aba501569c0dc0d59f9f420afd71c0cfea33
Vipin Sharma <vipinsharmaec@hotmail.com>
2018-03-24 21:00:49 +0530

    extract text plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,48 @@
 const webpack = require("webpack"),
-path = require("path");
+path = require("path"),
+ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
 
 module.exports ={
     entry:"./webpack.entry.js",
     output:{
         path:  path.resolve(__dirname, "public"),
-        filename:"bundle.js"
+        filename:"bundle.js",
+        publicPath:"public/"
     },
     module:{
         rules:[
             {
                 use:"babel-loader",
                 test:/\.js$/
+            },
+            {
+                loader: ExtractTextWebpackPlugin.extract({
+                    loader:["css-loader", "sass-loader"]
+                }),
+                test:/\.scss$/
+            },
+            {
+                loader: ExtractTextWebpackPlugin.extract({
+                    loader:["css-loader"]
+                }),
+                test:/\.css$/
+            },
+            {
+                use:[
+                    {
+                        loader:"url-loader",
+                        options:{
+                            limit:40000
+                        }
+                    },
+                    "image-webpack-loader"
+                ]
+                ,
+                test:/\.(jpe?g|png|gif|svg)$/
             }
         ]
-    }
+    },
+    plugins:[
+        new ExtractTextWebpackPlugin("style.css")
+    ]
 }
--- webpack.entry.js
+++ webpack.entry.js
@@ -1,1 +1,9 @@
+// import "./node_modules/bootstrap/dist/css/bootstrap.min.css";
+import big from "./assets/images/big.jpg";
+import small  from "./assets/images/small.jpg";
+import "./assets/css/style.scss";
 import "./app/app.module";
+
+let img = document.createElement("img");
+img.src =big;
+document.body.appendChild(img);
